19c0da99fbd987790c93a84c100f292c9d8fb949
user_name <Rezamohamadhoseini>
2020-10-11 20:22:57 +0330

    first push

--- Program.cs
+++ Program.cs
@@ -8,52 +8,55 @@ namespace app
     {
         static void inititalbrick()
         {
-            var PressedBRICK1 = new BRICK(Width = 10;
-            Lenght = 20;
-            Thickness = 5;)
+            var PressedBRICK1 = new BRICK(){
+                Width = 10,
+                Lenght = 20,
+                Thickness = 5
+            };
+        
     
     var PressedBRICK2 = new BRICK()
     {
-        Width = 11;
-            Lenght = 22;
-            Thickness = 5.5;
-        }
+        Width = 11,
+            Lenght = 22,
+            Thickness = 5.5
+        };
         BRICK MILITARYBRICK1 = new BRICK()
         {
-            Width = 50;
-        Lenght = 50;
-        Thickness = 5;
-    }
+            Width = 50,
+        Lenght = 50,
+        Thickness = 5
+    };
     var MILITARYBRICK2 = new BRICK()
     {
-        Width = 40;
-    Lenght = 40;
-        Thickness = 5;
-    }
+        Width = 40,
+    Lenght = 40,
+        Thickness = 5
+    };
 var ROOFBRICK = new BRICK()
 {
-    Width = 50;
-Lenght = 20;
-        Thickness = 25;
-    }
+    Width = 50,
+Lenght = 20,
+        Thickness = 25
+    };
   var BRICK3CM1 = new BRICK()
   {
-      Width = 22;
-Lenght = 11;
-    Thickness = 3;
-}
+      Width = 22,
+Lenght = 11,
+    Thickness = 3
+};
 var BRICK3CM2 = new BRICK()
 {
-    Width = 22;
-Lenght = 11;
-Thickness = 3.5;
-}
+    Width = 22,
+Lenght = 11,
+Thickness = 3.5
+};
 var BRICK3CM3 = new BRICK()
 {
-    Width = 22;
-Lenght = 11;
-Thickness = 4;
-}
+    Width = 22,
+Lenght = 11,
+Thickness = 4
+};
 
 
 
@@ -61,7 +64,7 @@ Thickness = 4;
 }
     static void main(string[] args)
 {
-
+inititalbrick;
 
 }
 
@@ -77,7 +80,7 @@ class BRICK
 {
     public float Width;
     public float Lenght;
-    public float Thickness;
+    public double Thickness;
 }
 
 class WALL
